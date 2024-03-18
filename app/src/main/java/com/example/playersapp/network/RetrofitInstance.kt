0a790a90bc683af367b7dd2abb8d05a19cf481d4
Android <dev123@gmail.com>
2024-03-18 17:36:26 +0530

    Add the source code for sequence and parallel network calls

--- app/src/main/java/com/example/playersapp/network/RetrofitInstance.kt
+++ app/src/main/java/com/example/playersapp/network/RetrofitInstance.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
 
-    private const val BASE_URL = "http://192.168.1.4:3002/"
+    private const val BASE_URL = "http://192.168.10.5:3002/"
 
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder().baseUrl(BASE_URL)
